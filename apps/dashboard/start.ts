46cbf2c8daebd34a5846244a191c6ec239e8b8ba
Danny Kim <0916dhkim@gmail.com>
2026-03-02 00:05:49 -0500

    comment

--- apps/dashboard/start.ts
+++ apps/dashboard/start.ts
@@ -1,3 +1,3 @@
-import "@secret-party/env/env";
+import "@secret-party/env/env"; // Load env vars and validate
 
-await import("./.output/server/index.mjs");
+await import("./.output/server/index.mjs"); // Run the built server
